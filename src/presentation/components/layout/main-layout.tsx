f5203db7799cf42c6d1a06e18f3b7b4e1bfe2a11
Janitha <janitha@ideabits.se>
2026-01-16 06:55:00 +0530

    main layout & images added

---
+++ src/presentation/components/layout/main-layout.tsx
@@ -0,0 +1,14 @@
+import Header from '../Header';
+import Footer from '../Footer';
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
